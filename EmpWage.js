1ef77aea67d1ebe2c86ae7d0b903c34f1b1aaa4e
dikoleshubh <dikole.shubh@gmail.com>
2021-06-05 12:50:37 +0530

    Added Array Function at UC-9

--- EmpWage.js
+++ EmpWage.js
@@ -16,7 +16,7 @@ let empCheck = Math.floor(Math.random()*10)%3;
     }
 }
 
-//UC2 Ability to Calculate Daily Employee Wage based on part time or full time work.
+//UC2 Ability to Calculate  Daily Employee Wage based on part time or full time work.
 {
 console.log("UC2 Calculating wage for employee");
 const IS_PART_TIME = 1;
